15e23917bd48145b171503cc20c78253ca42b836
rupanmentor <rupanguvimentor@gmail.com>
2025-12-02 19:47:18 +0530

    updated

---
+++ Middleware/adminMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import User from "../Models/userModel.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+export const adminMiddleware = async (req, res, next) => {
+  const token = req.header("Authorization");
+  if (!token) {
+    return res.status(404).json({ message: "Token Missing" });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    //console.log("decoded", decoded);
+    req.user = decoded;
+    const user = await User.findById(req.user._id);
+    if (user.role === "Admin") {
+      next();
+    } else {
+      res.status(404).json({ message: "Access denied only admin can view" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- Middleware/authMiddleware.js
+++ Middleware/authMiddleware.js
@@ -34,22 +34,3 @@ export const adminMiddleware = async (req, res, next) => {
   next();
 };
 */
-export const adminMiddleware = async (req, res, next) => {
-  const token = req.header("Authorization");
-  if (!token) {
-    return res.status(404).json({ message: "Token Missing" });
-  }
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    //console.log("decoded", decoded);
-    req.user = decoded;
-    const user = await User.findById(req.user._id);
-    if (user.role === "Admin") {
-      next();
-    } else {
-      res.status(404).json({ message: "Access denied only admin can view" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
