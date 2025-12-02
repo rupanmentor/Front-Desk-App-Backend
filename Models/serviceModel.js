4aed0896225c5a50ab1d6e74b569190055b04d59
rupanmentor <rupanguvimentor@gmail.com>
2025-12-02 19:02:19 +0530

    servicemodel completed

---
+++ Models/serviceModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const serviceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Service = mongoose.model("Service", serviceSchema);
+
+export default Service;
